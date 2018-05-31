13cb151d02e16f0d85261ccd897e44c94813a818
Maruf Nebil Ogunc <home@marufs-imac.local>
2018-05-31 14:00:26 +0300

    Initial Commit

---
+++ Pumped Up Kicks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pumped Up Kicks
+//
+//  Created by Maruf Nebil Ogunc on 31.05.2018.
+//  Copyright © 2018 Maruf Nebil Ogunc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pumped Up Kicks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pumped Up Kicks
+//
+//  Created by Maruf Nebil Ogunc on 31.05.2018.
+//  Copyright © 2018 Maruf Nebil Ogunc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
